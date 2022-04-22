c0774877075447d978d1b6d546c7dec0892a35f5
Armando Aranda <arandaae19@gmail.com>
2022-04-21 22:11:13 -0500

    First Commit

---
+++ Typewriter_Effect/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Typewriter_Effect
+{
+    class Program
+    {
+        static void Main()
+        {
+            Typewrite("Your text here...");
+            Console.ReadKey();  
+
+        }
+
+        static void Typewrite(string message)
+        {
+            for (int i = 0; i < message.Length; i++)
+            {
+                Console.Write(message[i]);
+                System.Threading.Thread.Sleep(60);
+            }
+        }
+    }
+}
